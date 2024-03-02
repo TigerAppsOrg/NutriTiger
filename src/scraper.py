13407ad3fb49191bd329fbbeea7e244d2edbf6d8
Eric Kurkowski <ek1074@princeton.edu>
2024-03-02 10:04:03 -0500

    Added menu information to JSON object. Key-value pairs need fixing

--- src/scraper.py
+++ src/scraper.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 """
@@ -20,25 +21,59 @@ Location 06: Yeh West
 """
 
 def main():
+    # Pre-defined constants, needs developer maintanance
+    LOCATION_NUMS = ["01", "03", "04", "05", "06", "08"]
+    LOCATION_DESCRIPTION = ["Rockefeller & Mathey Colleges", 
+                            "Forbes College", "Graduate College", 
+                            "Center for Jewish Life", 
+                            "Yeh & New West Colleges", 
+                            "Whitman & Butler Colleges"]
+    
+    # Complete JSON object to store the menus for one day
+    data = {}
+
+    # We can loop through all locations and obtain menu items as needed
+    for location_num, location_description in zip(LOCATION_NUMS, LOCATION_DESCRIPTION):
+        location_menu = get_daily_menu(location_num)
+        data[location_description] = location_menu
+
+    print(data)
+
+# ---------------------------------------------------------------------
+
+
+def get_daily_menu(location_num):
     # Will add a predefined location list and loop through each one to collect information
     base_menus_url = "https://menus.princeton.edu/dining/_Foodpro/online-menu/menu2.asp?"
-    # Pre-defined constant, needs developer maintanance
-    location_nums = ["01", "03", "04", "05", "06", "08"]
-    
-    # We can loop through all locations and obtain menu items as needed
-    for location_num in location_nums:
-        location_parameter = "locationNum=" + location_num
 
-        combined_url = base_menus_url + location_parameter
+    location_parameter = "locationNum=" + location_num
+    combined_url = base_menus_url + location_parameter
+
+    data = {}
+
+    try:
         request = requests.get(combined_url)
-        if request.status_code != 200:
-            raise Exception("Error retrieving URL")
-        
-        print(request.content)
         xml_content = request.content
 
         soup = BeautifulSoup(xml_content, "xml")
-        print(soup.find("name"))
+        mealsList = soup.find_all("meal")
+
+        for meal in mealsList:
+            mealType = meal.get_attribute_list("name")[0]
+            data["meal"] = mealType
+            entreeList = meal.find_all("entree")
+
+            for entree in entreeList:
+                entreeDescription = entree.get_attribute_list("type")[0][3:-3]
+                data["entreeDescription"] = entreeDescription
+                data["entreeName"] = entree.find("name").contents[0]
+                data["recipeNumber"] = entree.find("recnum").contents[0]
+                data["allergens"] = entree.find("allergens").contents[0]
+        
+        return data
+    
+    except Exception as ex:
+        print(sys.argv[0] + ":", ex, file=sys.stderr)
 
 if __name__ == "__main__":
     main()
