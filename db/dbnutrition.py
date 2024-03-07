472d1ac90e0f33be1d8b0cede08039c72b9e6b88
oyu-e <oe7583@princeton.edu>
2024-03-07 12:06:14 -0500

    outline db functions

--- db/dbnutrition.py
+++ db/dbnutrition.py
@@ -1,1 +1,31 @@
+import pymongo
 
+#----------------------------------------------------------------------
+#
+# Oyu Enkhbol and Jewel Merriman
+#
+#----------------------------------------------------------------------
+
+# Create new food item
+def newfood():
+    return
+
+# Create new food item - specific to user
+def newpersonalfood():
+    return
+
+# INSIDE UPDATE MENU: delete outdated food entries
+def __deletemenu__():
+    return
+
+# Update dining hall menus for the next two weeks --> put inside db
+def updatemenu():
+    return
+
+# Retrive food items for the next week (dhall display page)
+def querymenu():
+    return
+
+# Retrieve food that a specific user has consumed during the day
+def querytoday():
+    return
--- db/dbusers.py
+++ db/dbusers.py
@@ -1,1 +1,35 @@
+import pymongo
 
+#----------------------------------------------------------------------
+#
+# Oyu Enkhbol and Jewel Merriman
+#
+#----------------------------------------------------------------------
+
+# Create new user profile: include calorie count, personal info
+def newuser():
+    return
+
+# Update user's calorie goal
+def updategoal():
+    return
+
+# INSIDE UPDATEDCONSUMED: organize macro/calorie count:
+def __updatenutrition__():
+    return
+
+# Update user's consumed good: add to daily plate
+def addconsumed():
+    return
+
+# Update user's consumed good: delete entry from daily plate
+def deleteconsumed():
+    return
+
+# Retrive user's profile (including name/netid, goals, consumed)
+def finduser():
+    return
+
+# delete user's data entries
+def deleteuser():
+    return
