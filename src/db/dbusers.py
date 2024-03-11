8fb9dc5b51a4e66c5dfee8b48efc4fa8c0b1370d
Jewel Merriman <jewel@Jewels-MBP.home.lla.com>
2024-03-11 17:24:41 -0400

    testing connection to mongodb cluster

--- src/db/dbusers.py
+++ src/db/dbusers.py
@@ -1,5 +1,8 @@
 import pymongo
-
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import ssl
+import os
 #----------------------------------------------------------------------
 # Contributors:
 # Jewel Merriman and Oyu Enkhbold
@@ -33,3 +36,30 @@ def finduser():
 # delete user's data entries
 def deleteuser():
     return
+
+# to test connection
+def main(): 
+
+    # BEFORE YOU MUST export your mongodb username and password to environment 
+    password = os.getenv("MONGODB_PASSWORD")
+    username = os.getenv("MONGODB_USERNAME")
+
+    uri = f"mongodb+srv://{username}:{password}@aws-m0-cluster.cywlmar.mongodb.net/?retryWrites=true&w=majority&appName=aws-m0-cluster"
+
+    # Create a new client and connect to the server
+    try: 
+        client = MongoClient(uri)
+    except Exception as e:
+        print(e)
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
