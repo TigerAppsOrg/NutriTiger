1343c1025a467e9877ac099d2d0abb42d3ff7d8c
oyu-e <oe7583@princeton.edu>
2024-03-13 19:07:43 -0400

    distinguishing dbmenus and dbnutrition / also putting connectmongo in dbfunctions to avoid repeat code

--- src/db/dbnutrition.py
+++ src/db/dbnutrition.py
@@ -1,5 +1,6 @@
 import pymongo
-
+from dbfunctions import connectmongo
+import sys
 #----------------------------------------------------------------------
 # Contributors:
 # Oyu Enkhbold and Jewel Merriman
@@ -7,12 +8,25 @@ import pymongo
 #----------------------------------------------------------------------
 
 # Create new food item
-def newfood():
+def newfood(new_food):
+    db = connectmongo()
+    nutrition_col = db.nutrition
+    result = nutrition_col.insert_one(new_food)
+    document_id = result.inserted_id
+    try:
+        nutrition_col.insert_one(new_food)
+    except pymongo.errors.OperationFailure:
+        print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
+        sys.exit(1)
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
+        sys.exit(1)
+    print(f"_id of inserted document: {document_id}")
     return
 
-# Create new food item - specific to user
-def newpersonalfood():
-    return
+# Create new food item - specific to user (this is specific to entry)
+# def newpersonalfood(new_food):
+
 
 # INSIDE UPDATE MENU: delete outdated food entries
 def __deletemenu__():
--- src/db/dbusers.py
+++ src/db/dbusers.py
@@ -2,9 +2,11 @@ import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from datetime import datetime
+from dbfunctions import connectmongo
 import bson
 import ssl
 import os
+import sys
 #----------------------------------------------------------------------
 # Contributors:
 # Jewel Merriman and Oyu Enkhbold
@@ -12,24 +14,6 @@ import os
 # Based off of atlas starter packet: 
 # https://github.com/mongodb-university/atlas_starter_python/blob/master/atlas-starter.py
 #----------------------------------------------------------------------
-
-# Connect to NutriTiger MongoDB database and return database object
-def connectmongo():
-    password = os.getenv("MONGODB_PASSWORD")
-    username = os.getenv("MONGODB_USERNAME")
-
-    uri = f"mongodb+srv://{username}:{password}@aws-m0-cluster.cywlmar.mongodb.net/?retryWrites=true&w=majority&appName=aws-m0-cluster"
-    try:
-        client = pymongo.MongoClient(uri)
-  
-    # return a friendly error if a URI error is thrown 
-    except pymongo.errors.ConfigurationError:
-        print("An Invalid URI host error was received. Make sure your connection string is valid. See README for more details.")
-        sys.exit(1)
-
-    # return database named "NT-db"
-    return client['NT-db']
-
 
 # Create new user profile: include calorie count, personal info
 def newuser(name, netid, bday, cal):
