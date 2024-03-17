c6175cac70bbc193dbd76f5ba288efc9549b9b0a
oyu-e <oe7583@princeton.edu>
2024-03-17 18:50:08 -0400

    all that is left is testing

--- src/db/dbnutrition.py
+++ src/db/dbnutrition.py
@@ -55,12 +55,21 @@ def find_one_nutrition(recipeid):
             sys.exit(1)
 
 # Create new food item -- should already include the net id field of user
-def addpersonalfood(name, nutrition):
+def addpersonalfood(name, netid, nutrition):
     with connectmongo() as client:
         db = client.db
         nutrition_col = db.nutrition
-        
-        new_food = {"mealname" : name, 
+        current_items = {"netid": netid}
+
+        try:
+            check = nutrition_col.find(current_items)
+            document_ids = check.inserted_ids
+            num_current = str(len(document_ids))
+            recipeid = num_current + 1
+            try:
+                new_food = {"mealname" : name, 
+                    "netid": netid,
+                    "recipeid" : recipeid,
                     "calories" : nutrition[0], 
                     "proteins" : nutrition[1], 
                     "carbs" : nutrition[2],
@@ -76,12 +85,12 @@ def addpersonalfood(name, nutrition):
                     "allergen": nutrition[12],
                     "ingredients": nutrition[13]
                     }
-
-        try:
-            result = nutrition_col.insert_one(new_food)
-            document_id = result.inserted_id
-            print(f"_id of inserted document: {document_id}")
-            return
+                result = nutrition_col.insert_one(new_food)
+                document_id = result.inserted_id
+                print(f"_id of inserted document: {document_id}")
+                return
+            except:
+                print("issue with insert")
         except pymongo.errors.OperationFailure:
             print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
             sys.exit(1)
@@ -107,7 +116,7 @@ def find_many_nutrition(recipeids):
             sys.exit(1)
 
 # Retrieve nutritional information of a user
-def find_personal_nutrition(netid):
+def find_all_personal_nutrition(netid):
     with connectmongo() as client:
         db = client.db
         nutrition_col = db.nutrition
@@ -124,4 +133,20 @@ def find_personal_nutrition(netid):
             print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
             sys.exit(1)
 
-# find one personal nutrition info
+# Find one personal nutrition info with user's netid and recipeid
+def find_one_personal_nutrition(netid, recipeid):
+    with connectmongo() as client:
+        db = client.db
+        nutrition_col = db.nutrition
+
+        documents_to_find = {"access": netid, "recipeid": recipeid}
+        try:
+            result = nutrition_col.find_one(documents_to_find)
+            print(f"found documents: {result}")
+            return result
+        except pymongo.errors.OperationFailure:
+            print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
+            sys.exit(1)
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
+            sys.exit(1)            
