5389ab9ca3e8d1268638215b1a9823195b98bfad
oyu-e <oe7583@princeton.edu>
2024-03-17 18:31:03 -0400

    again

--- src/db/dbmenus.py
+++ src/db/dbmenus.py
@@ -9,41 +9,24 @@ import pytz
 #
 #----------------------------------------------------------------------
 
-# INSIDE UPDATE MENU: delete food entries prior to the start of this week (Monday)
-def __deletemenu__():
-    with connectmongo() as client:
-        db = client.db
-        menus_col = db.menus
-        eastern_time = pytz.timezone('US/Eastern')
-        today = datetime.today(eastern_time)
-        day_of_week = today.weekday()
-        dt = today - datetime.timedelta(days = day_of_week)
-
-        documents_to_delete = {"date": {"$lt": dt}}
-        try:
-            result = menus_col.delete_many(documents_to_delete)
-            print(f"# of deleted documents: {result.deleted_count}")
-            return
-        except pymongo.errors.OperationFailure:
-            print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
-            sys.exit(1)
-        except pymongo.errors.ServerSelectionTimeoutError:
-            print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
-            sys.exit(1)
-
-
-# Update dining hall menus for the next two weeks --> accepts list as paramter
+# Delete food entires prior to today and updates dining hall menus for the next two weeks 
 def updatemenu(menu_list):
-    __deletemenu__()
-
     with connectmongo() as client:
         db = client.db
         menu_col = db.menus
+
+        eastern_time = pytz.timezone('US/Eastern')
+        today = datetime.today(eastern_time)
+
+        documents_to_delete = {"date": {"$lt": today}}
+
         try:
-            result = menu_col.insert_many(menu_list)
-            document_ids = result.inserted_ids
+            delete_result = menu_col.delete_many(documents_to_delete)
+            print(f"# of deleted documents: {delete_result.deleted_count}")
+            add_result = menu_col.insert_many(menu_list)
+            document_ids = add_result.inserted_ids
             print(f"# of documents inserted: {str(len(document_ids))}")
-            print(f"_id of inserted document: {document_ids}")
+            print(f"_id of inserted documents: {document_ids}")
             return
         except pymongo.errors.OperationFailure:
             print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
@@ -52,22 +35,18 @@ def updatemenu(menu_list):
             print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
             sys.exit(1)
 
-
-# Retrive food items for the next week (dhall display page)
-def querymenudisplay(dhall):
+# Retrive food items for menu by date, mealtime, and dhall (optional)
+def querymenudisplay(date, mealtime, dhall = None):
     with connectmongo() as client:
         db = client.db
         menu_col = db.menus
 
-        # determine start and end of the week
-        eastern_time = pytz.timezone('US/Eastern')
-        today = datetime.today(eastern_time)
-        day_of_week = today.weekday()
-        start_of_week = today - datetime.timedelta(days = (day_of_week - 1))
-        end_of_week = today + datetime.timedelta(days = (7 - day_of_week))
         
-        documents_to_find = {"date": {"$gt": start_of_week, "$lt": end_of_week},
-                             "dhall": {"$eq": dhall}}
+        if dhall is None:
+            documents_to_find = {"date": {"$eq": date}, "mealtime": {"$eq": mealtime}}
+        else:
+            documents_to_find = {"date": {"$eq": date}, "mealtime": {"$eq": mealtime}, "dhall":{"$eq": dhall}}
+
         try:
             result = menu_col.find(documents_to_find)
             print(f"found documents: {result}")
@@ -78,27 +57,11 @@ def querymenudisplay(dhall):
         except pymongo.errors.ServerSelectionTimeoutError:
             print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
             sys.exit(1)
+            
+# Testing
+def main():
+    newmenu = {
 
-
-# Retrive food items for today's menu at dhall
-def querymenutoday(dhall):
-    with connectmongo() as client:
-        db = client.db
-        menu_col = db.menus
-
-        # determine start and end of the week
-        eastern_time = pytz.timezone('US/Eastern')
-        today = datetime.today(eastern_time)
-        
-        documents_to_find = {"date": {"$eq": today},
-                             "dhall": {"$eq": dhall}}
-        try:
-            result = menu_col.find(documents_to_find)
-            print(f"found documents: {result}")
-            return result
-        except pymongo.errors.OperationFailure:
-            print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
-            sys.exit(1)
-        except pymongo.errors.ServerSelectionTimeoutError:
-            print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
-            sys.exit(1)
+    }
+    sys.exit(0)
+    
--- src/db/dbnutrition.py
+++ src/db/dbnutrition.py
@@ -9,33 +9,24 @@ import pytz
 #
 #----------------------------------------------------------------------
 
-# Create new food item -- should already include the net id field of user
-def addpersonalfood(new_food):
+# Deletes all nutrition information of food items prior to today and
+# inserts nutrition information of the upcoming two weeks of food (as input)
+def update_nutrition(new_foods):
     with connectmongo() as client:
         db = client.db
         nutrition_col = db.nutrition
+        eastern_time = pytz.timezone('US/Eastern')
+        eastern_time = pytz.timezone('US/Eastern')
+        today = datetime.today(eastern_time)
+        day_of_week = today.weekday()
+        dt = today - datetime.timedelta(days = day_of_week)
+        documents_to_delete = {"date": {"$lt": dt}}
         
         try:
-            result = nutrition_col.insert_one(new_food)
-            document_id = result.inserted_id
-            print(f"_id of inserted document: {document_id}")
-            return
-        except pymongo.errors.OperationFailure:
-            print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
-            sys.exit(1)
-        except pymongo.errors.ServerSelectionTimeoutError:
-            print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
-            sys.exit(1)
-
-# Inserts nutrition information of the upcoming two weeks of food
-def addmenunutrition(new_foods):
-    with connectmongo() as client:
-        db = client.db
-        nutrition_col = db.nutrition
-        
-        try:
-            result = nutrition_col.insert_many(new_foods)
-            document_ids = result.inserted_ids
+            delete_result = nutrition_col.delete_many(documents_to_delete)
+            print(f"# of deleted documents: {delete_result.deleted_count}")
+            add_result = nutrition_col.insert_many(new_foods)
+            document_ids = add_result.inserted_ids
             print(f"_id of inserted documents: {document_ids}")
             return
         except pymongo.errors.OperationFailure:
@@ -46,29 +37,6 @@ def addmenunutrition(new_foods):
             sys.exit(1)
         
 
-# Deletes all nutrition information of food items prior to this week
-def deletenutritioninfo():
-    with connectmongo() as client:
-        db = client.db
-        nutrition_col = db.nutrition
-        eastern_time = pytz.timezone('US/Eastern')
-        eastern_time = pytz.timezone('US/Eastern')
-        today = datetime.today(eastern_time)
-        day_of_week = today.weekday()
-        dt = today - datetime.timedelta(days = day_of_week)
-
-        documents_to_delete = {"date": {"$lt": dt}}
-        try:
-            result = nutrition_col.delete_many(documents_to_delete)
-            print(f"# of deleted documents: {result.deleted_count}")
-            return
-        except pymongo.errors.OperationFailure:
-            print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
-            sys.exit(1)
-        except pymongo.errors.ServerSelectionTimeoutError:
-            print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
-            sys.exit(1)
-        
 # Retrieve nutritional information of singular food item
 def find_one_nutrition(recipeid):
     with connectmongo() as client:
@@ -87,12 +55,46 @@ def find_one_nutrition(recipeid):
             print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
             sys.exit(1)
 
+# Create new food item -- should already include the net id field of user
+def addpersonalfood(name, nutrition):
+    with connectmongo() as client:
+        db = client.db
+        nutrition_col = db.nutrition
+        
+        new_food = {"mealname" : name, 
+                    "calories" : nutrition[0], 
+                    "proteins" : nutrition[1], 
+                    "carbs" : nutrition[2],
+                    "fats" : nutrition[3],
+                    "chloesterol" : nutrition[4],
+                    "sodium" : nutrition[5],
+                    "calcium" : nutrition[6],
+                    "vitd" : nutrition[7],
+                    "potassium" : nutrition[8],
+                    "iron" : nutrition[9],
+                    "sugar" : nutrition[10],
+                    "fiber" : nutrition[11],
+                    "allergen": nutrition[12],
+                    "ingredients": nutrition[13]
+                    }
+
+        try:
+            result = nutrition_col.insert_one(new_food)
+            document_id = result.inserted_id
+            print(f"_id of inserted document: {document_id}")
+            return
+        except pymongo.errors.OperationFailure:
+            print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
+            sys.exit(1)
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
+            sys.exit(1)
+
 # Retrieve nutritional information of multiple food items based on recipeids
 def find_many_nutrition(recipeids):
     with connectmongo() as client:
         db = client.db
         nutrition_col = db.nutrition
-
         documents_to_find = {"recipeid": recipeids}
         try:
             result = nutrition_col.find(documents_to_find)
@@ -104,3 +106,9 @@ def find_many_nutrition(recipeids):
         except pymongo.errors.ServerSelectionTimeoutError:
             print("The server timed out. Is your IP address added to Access List? To fix this, add your IP address in the Network Access panel in Atlas.")
             sys.exit(1)
+
+# Testing
+def main():
+    
+    sys.exit(0)
+    
