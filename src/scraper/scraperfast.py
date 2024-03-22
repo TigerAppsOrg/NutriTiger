3d9d369a18f634a189c71ca56733af3983beb6ff
Eric Kurkowski <ek1074@princeton.edu>
2024-03-22 12:18:40 -0400

    Initial scraper implementation with concurrency

--- src/scraper/scraperfast.py
+++ src/scraper/scraperfast.py
@@ -13,6 +13,7 @@ Using manual scraper over MobileApp API to obtain entree descriptions
 Sources Used:
 https://www.geeksforgeeks.org/python-web-scraping-tutorial/
 https://towardsdatascience.com/xml-scraping-done-right-6ac66eef9efc
+https://realpython.com/python-concurrency/
 
 Location 05: CJL
 Location 03: Forbes
@@ -26,8 +27,10 @@ import requests
 import sys
 import datetime
 import json
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
-import webscraper
+import webscraperfast
 
 # ---------------------------------------------------------------------
 
@@ -53,7 +56,7 @@ def main():
     in a preferred format and print it to stdout.
     """
 
-    todays_menu_items, todays_nutrition_list = get_daily_menus()
+    todays_menu_items, todays_nutrition_list = asyncio.run(get_daily_menus())
     print(todays_menu_items)
     print(todays_nutrition_list)
 
@@ -104,7 +107,7 @@ def get_daily_menus_from_range(start_date, end_date):
 # ---------------------------------------------------------------------
 
 
-def get_daily_menus(date=""):
+async def get_daily_menus(date=""):
     """
     Based on the predefined location numbers in LOCATION_NUMS and
     descriptions in LOCATION_DESCRIPTION get_daily_menus obtains the
@@ -119,23 +122,25 @@ def get_daily_menus(date=""):
     distinct_recipeid_list = []
 
     # Obtain the menu items from each location
-    for location_num, location_description in zip(LOCATION_NUMS, LOCATION_DESCRIPTION):
-        location_menu, nutrition_list = get_daily_menu(location_num, location_description, distinct_recipeid_list, date)
+    async with aiohttp.ClientSession() as session:
+        job_results = []
+        for location_num, location_description in zip(LOCATION_NUMS, LOCATION_DESCRIPTION):
+            job = asyncio.ensure_future(get_daily_menu(location_num, location_description, distinct_recipeid_list, session, date))
+            job_results.append(job)
+        await asyncio.gather(*job_results, return_exceptions=True)
 
-        # Unpacks the list of entree_type entries and adds to the complete list
-        for location_menu_entree_type in location_menu:
-            complete_menu_data_list.append(location_menu_entree_type)
+        # Can cause error if no data
+        for result in job_results:
+            for obj in result.result():
+                for test in obj:
+                    complete_menu_data_list.append(test)
 
-        # Unpacks the list of nutrition infromation entries and adds to the complete list
-        for location_nutrition_data_item in nutrition_list:
-            complete_nutrition_data_list.append(location_nutrition_data_item)
-    
     return complete_menu_data_list, complete_nutrition_data_list
 
 # ---------------------------------------------------------------------
 
 
-def get_daily_menu(location_num, location_description, distinct_recipeid_list, date=""):
+async def get_daily_menu(location_num, location_description, distinct_recipeid_list, session, date=""):
     """
     For a specific location, we make a request for the XML with the
     menu details and create JSON objects based on the unique entree
@@ -157,80 +162,94 @@ def get_daily_menu(location_num, location_description, distinct_recipeid_list, d
     # Attempt to make the request to create JSON objects from the menu
     try:
         # Make the request and save the content
-        request = requests.get(COMBINED_URL)
-        xml_content = request.content
+        async with session.get(COMBINED_URL) as request:
+            #request = requests.get(COMBINED_URL)
+            #print(request.text())
+            #xml_content = request.content
+            xml_content = await request.text()
+            print(request)
+            print("STATUS TEST:", request.status)
+            print(xml_content)
 
-        # Parse the content and find all meals:
-        # (Breakfast, Lunch, Dinner)
-        soup = BeautifulSoup(xml_content, "xml")
-        mealsList = soup.find_all("meal")
+            # Parse the content and find all meals:
+            # (Breakfast, Lunch, Dinner)
+            soup = BeautifulSoup(xml_content, "xml")
+            mealsList = soup.find_all("meal")
 
-        # Final list will all JSON objects to return
-        complete_menu_data_list = []
-        complete_nutrition_data_list = []
+            # Final list will all JSON objects to return
+            complete_menu_data_list = []
+            complete_nutrition_data_list = []
 
-        # Create JSON objects based on entree type for each meal
-        for meal in mealsList:
-            # Obtain the list of entries for that meal
-            mealType = meal.get_attribute_list("name")[0]
-            entreeList = meal.find_all("entree")
-            
-            # Stores the JSON object for that entree type
-            data = {}
-            # Stores the items in that entree type
-            food_items = []
-            # Stores the recipe number for each entree type
-            recipe_nums = []
-            # Stores the previous entree description to combine
-            # identical entree types for a meal
-            oldEntreeDescription = ""
+            async with aiohttp.ClientSession() as session2:
+                tasks = []
 
-            # Go through each entree and add to the JSON object or
-            # start a new one
-            for i, entree in enumerate(entreeList):
-                # Get the description of the entree
-                entreeDescription = entree.get_attribute_list("type")[0][3:-3]
+                # Create JSON objects based on entree type for each meal
+                for meal in mealsList:
+                    # Obtain the list of entries for that meal
+                    mealType = meal.get_attribute_list("name")[0]
+                    entreeList = meal.find_all("entree")
+                    
+                    # Stores the JSON object for that entree type
+                    data = {}
+                    # Stores the items in that entree type
+                    food_items = []
+                    # Stores the recipe number for each entree type
+                    recipe_nums = []
+                    # Stores the previous entree description to combine
+                    # identical entree types for a meal
+                    oldEntreeDescription = ""
 
-                # If its not the first iteration and we are on a new
-                # entree description, start a new JSON object
-                if i > 0:
-                    if entreeDescription != oldEntreeDescription:
-                        # Save the data before clearing
-                        data["fooditems"] = food_items
-                        data["recipenums"] = recipe_nums
-                        complete_menu_data_list.append(data)
+                    # Go through each entree and add to the JSON object or
+                    # start a new one
+                    for i, entree in enumerate(entreeList):
+                        # Get the description of the entree
+                        entreeDescription = entree.get_attribute_list("type")[0][3:-3]
+
+                        # If its not the first iteration and we are on a new
+                        # entree description, start a new JSON object
+                        if i > 0:
+                            if entreeDescription != oldEntreeDescription:
+                                # Save the data before clearing
+                                data["fooditems"] = food_items
+                                data["recipenums"] = recipe_nums
+                                complete_menu_data_list.append(data)
+                                
+                                # Clear the data
+                                data = {}
+                                food_items = []
+                                recipe_nums = []
                         
-                        # Clear the data
-                        data = {}
-                        food_items = []
-                        recipe_nums = []
+                        # If we are on a new data object, add the needed fields
+                        if len(data) == 0:
+                            if date != "":
+                                data["date"] = date
+                            else:
+                                data["date"] = datetime.datetime.today()
+                            data["dhall"] = location_description
+                            data["mealtime"] = mealType
+                            data["type"] = entreeDescription
+
+                        # Update the old entree and append the items to the JSON
+                        oldEntreeDescription = entreeDescription
+                        food_items.append(entree.find("name").text)
+                        recipeid = entree.find("recnum").text
+                        recipe_nums.append(recipeid)
+
+                        # Add the nutrition information for the recipe number to the list
+                        if recipeid not in distinct_recipeid_list:
+                            distinct_recipeid_list.append(recipeid)
+                            job = asyncio.ensure_future(webscraperfast.get_nutrition_from_recipe(recipeid, session2))
+                            tasks.append(job)
+                            #complete_nutrition_data_list.append(webscraper.get_nutrition_from_recipe(recipeid))
                 
-                # If we are on a new data object, add the needed fields
-                if len(data) == 0:
-                    if date != "":
-                        data["date"] = date
-                    else:
-                        data["date"] = datetime.datetime.today()
-                    data["dhall"] = location_description
-                    data["mealtime"] = mealType
-                    data["type"] = entreeDescription
+                    data["fooditems"] = food_items
+                    data["recipenums"] = recipe_nums
+                    complete_menu_data_list.append(data)
 
-                # Update the old entree and append the items to the JSON
-                oldEntreeDescription = entreeDescription
-                food_items.append(entree.find("name").text)
-                recipeid = entree.find("recnum").text
-                recipe_nums.append(recipeid)
-
-                # Add the nutrition information for the recipe number to the list
-                if recipeid not in distinct_recipeid_list:
-                    distinct_recipeid_list.append(recipeid)
-                    complete_nutrition_data_list.append(webscraper.get_nutrition_from_recipe(recipeid))
-        
-            data["fooditems"] = food_items
-            data["recipenums"] = recipe_nums
-            complete_menu_data_list.append(data)
-
-        return complete_menu_data_list, complete_nutrition_data_list
+                await asyncio.gather(*tasks, return_exceptions=True)
+                
+                complete_nutrition_data_list = tasks
+                return complete_menu_data_list, complete_nutrition_data_list
 
     except Exception as ex:
         print(sys.argv[0] + ":", ex, file=sys.stderr)
---
+++ src/scraper/webscraperfast.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+
+# ----------------------------------------------------------------------
+# webscraper.py
+# Author: Eric
+# ----------------------------------------------------------------------
+
+import requests
+import json
+import re
+import sys
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+
+# ---------------------------------------------------------------------
+
+# Pre-defined constants, may need developer maintanance if html format
+# changes (unlikely)
+
+# Base URL to retrive the html for the menu item
+BASE_LABEL_URL = "https://menus.princeton.edu/dining/_Foodpro/online-menu/label.asp?RecNumAndPort="
+CALORIES_INDEX = 1
+CALORIES_OFFSET = 9
+PROTEINS_OFFSET = 8
+CARBS_OFFSET = 11
+FATS_OFFSET = 10
+CHOLESTEROL_OFFSET = 12
+SODIUM_OFFSET = 7
+SUGAR_OFFSET = 9
+FIBER_OFFSET = 16
+VITAMIND_OFFSET = 14
+POTASSIUM_OFFSET = 11
+CALCIUM_OFFSET = 9
+IRON_OFFSET = 6
+INGREDIENTS_CLASS = "labelingredientsvalue"
+ALLERGEN_CLASS = "labelallergensvalue"
+
+# ---------------------------------------------------------------------
+
+
+def main():
+    """
+    The main function uses the other functions defined in this file to
+    obtain a JSON object with nutrition information of a menu item in a preferred
+    format and prints it to stdout.
+    """
+
+    # A sample recipeid to obtain nutrition information from
+    SAMPLE_RECIPEID = 530602
+
+    # Obtains the nutrition information
+    nutrition_information = get_nutrition_from_recipe(SAMPLE_RECIPEID)
+    
+    # Prints the nutrition information nicely to stdout
+    print(json.dumps(nutrition_information, indent=4, default=str))
+
+# ---------------------------------------------------------------------
+
+
+async def get_nutrition_from_recipe(recipeid, session):
+    """
+    Based on the provided recipeid get_nutrition_from_recipe obtains
+    the nutritional information from the html of the nutrition website
+    and returns the created JSON object.
+    """
+
+    # Attempt to make the request to a create JSON object from the recipeid
+    try:
+        # Generate the URL to make the request
+        COMBINED_URL = BASE_LABEL_URL + str(recipeid)
+
+        # Make the request and save the content
+        async with session.get(COMBINED_URL) as request:
+            #request = requests.get(COMBINED_URL)
+            html_content = await request.text()
+            #html_content = request.content
+
+            # Parse the content and create the nutrition object to return
+            soup = BeautifulSoup(html_content, "html.parser")
+            nutrition_json = {}
+
+            # Insert the recipeid
+            nutrition_json["recipeid"] = recipeid
+            # Insert the name of the item, which is the first h2 tag
+            nutrition_json["mealname"] = soup.find("h2").text
+            # Insert the link to the nutritional information
+            nutrition_json["link"] = COMBINED_URL
+
+            # If there is no data to parse, return an object with no information
+            if soup.find(string=re.compile('Nutritional Information is not available for this recipe.')):
+                nutrition_json["calories"] = ""
+                nutrition_json["fats"] = ""
+                nutrition_json["carbs"] = ""
+                nutrition_json["fiber"] = ""
+                nutrition_json["suger"] = ""
+                nutrition_json["cholesterol"] = ""
+                nutrition_json["proteins"] = ""
+                nutrition_json["sodium"] = ""
+                nutrition_json["vitd"] = ""
+                nutrition_json["potassium"] = ""
+                nutrition_json["calcium"] = ""
+                nutrition_json["iron"] = ""
+                nutrition_json["ingredients"] = ""
+                nutrition_json["allergen"] = ""
+                return nutrition_json
+
+            # Insert the calories, which is the second sequential element with id facts2
+            calories_element = soup.find_all(id="facts2")[CALORIES_INDEX]
+            nutrition_json["calories"] = parse_nutrition_value("calories", calories_element)
+
+            # Obtains entries from the table that represents a nutrition label
+            nutrition_table_details = soup.find_all(id="facts4")
+
+            # For each element in the table, extract the number for each item
+            for element in nutrition_table_details:
+                # The title for what nutritional information a value represents is usaully a child element
+                title = element.findChild()
+
+                # Extract the quantity for each item in the nutrition table
+                if title is not None:
+                    title = title.text.strip()
+                    if title == "Protein":
+                        nutrition_json["proteins"] = parse_nutrition_value("proteins", element)
+                    if title == "Tot. Carb.":
+                        nutrition_json["carbs"] = parse_nutrition_value("carbs", element)
+                    if title == "Total Fat":
+                        nutrition_json["fats"] = parse_nutrition_value("fats", element)
+                    if title == "Cholesterol":
+                        nutrition_json["cholesterol"] = parse_nutrition_value("cholesterol", element)
+                    if title == "Sodium":
+                        nutrition_json["sodium"] = parse_nutrition_value("sodium", element)
+
+                # Extract the quantity for items with no seperate title element
+                if element.text.strip().startswith("Sugars"):
+                    nutrition_json["sugar"] = parse_nutrition_value("sugar", element)
+                if element.text.strip().startswith("Dietary Fiber"):
+                    nutrition_json["fiber"] = parse_nutrition_value("fiber", element)
+
+            # Obtains entries from the list in the nutrition label
+            nutrition_table_list = soup.find_all("li")
+
+            # For each entry in the list extract the value from the text
+            for element in nutrition_table_list:
+                element = element.text.strip()
+                if element.startswith("Vitamin D"):
+                    nutrition_json["vitd"] = element.replace(" ", "")[VITAMIND_OFFSET:]
+                if element.startswith("Potassium"):
+                    nutrition_json["potassium"] = element.replace(" ", "")[POTASSIUM_OFFSET:]
+                if element.startswith("Calcium"):
+                    nutrition_json["calcium"] = element.replace(" ", "")[CALCIUM_OFFSET:]
+                if element.startswith("Iron"):
+                    nutrition_json["iron"] = element.replace(" ", "")[IRON_OFFSET:]
+
+            # Saves the ingredients and allergens list as a Python list to the JSON object
+            nutrition_json["ingredients"] = soup.find("span", {"class": INGREDIENTS_CLASS}).text
+            nutrition_json["allergen"] = soup.find("span", {"class": ALLERGEN_CLASS}).text
+
+            return nutrition_json
+    
+    except Exception as ex:
+        print(sys.argv[0] + ":", ex, file=sys.stderr)
+
+# ---------------------------------------------------------------------
+
+
+def parse_nutrition_value(key, element):
+    """
+    Given a key that corresponds to a nutrition item on a nutrition label
+    and corresponding html element from the meal nutrition website,
+    parse_nutrition_value returns the parsed string containing the value.
+    An empty string is returned if there is no entry for an item.
+    """
+    
+    value = ""
+
+    match key:
+        case "calories":
+            value = element.text[CALORIES_OFFSET:]
+        case "proteins":
+            value = element.text[PROTEINS_OFFSET:]
+        case "carbs":
+            value = element.text[CARBS_OFFSET:]
+        case "fats":
+            value = element.text[FATS_OFFSET:]
+        case "cholesterol":
+            value = element.text[CHOLESTEROL_OFFSET:]
+        case "sodium":
+            value = element.text[SODIUM_OFFSET:]
+        case "calcium":
+            value = element.text[CALCIUM_OFFSET:]
+        case "vitd":
+            value = element.text[VITAMIND_OFFSET:]
+        case "potassium":
+            value = element.text[POTASSIUM_OFFSET:]
+        case "iron":
+            value = element.text[IRON_OFFSET:]
+        case "sugar":
+            value = element.text[SUGAR_OFFSET:]
+        case "fiber":
+            value = element.text[FIBER_OFFSET:]
+            
+    if value.startswith("-"):
+        return ""
+    else:
+        return value
+
+if __name__ == "__main__":
+    main()
