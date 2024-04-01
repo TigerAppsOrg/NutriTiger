c6ce39b9e846041f96dfcccfc503e2979ada9f67
Rishabh Jain <rishabh32jain@gmail.com>
2024-04-01 14:09:18 -0400

    updating script

--- src/dhallmenus.py
+++ src/dhallmenus.py
@@ -13,7 +13,7 @@ import asyncio
 def main():
     start_date = datetime.datetime.today()
     start_date_zeros = datetime.datetime(start_date.year, start_date.month, start_date.day)
-    end_date = start_date_zeros + datetime.timedelta(days=1)
+    end_date = start_date_zeros + datetime.timedelta(days=2)
     #start_date_str = start_date..strftime('%Y-%m-%d')
     #end_date_str = end_date.strftime('%Y-%m-%d')
     menu_items_list_range, menu_items_nutrition_range = asyncio.run(get_daily_menus_from_range(start_date_zeros, end_date))
@@ -24,9 +24,16 @@ def main():
     #    outfile.write(json_object)
 
 
-    #update_menu(menu_items_list_range)
+    update_menu(menu_items_list_range)
     print(menu_items_nutrition_range)
     update_nutrition(menu_items_nutrition_range)
+    #print(type(menu_items_nutrition_range))
+    '''for item in menu_items_list_range:
+        print(item)
+        if item is not None:
+            update_nutrition(item)
+        else:
+            print("NONE?")'''
 
 
 if __name__ == "__main__":
