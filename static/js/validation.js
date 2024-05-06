44523acfa6dd8fd7042e02aa24e5a993cc454e1c
Stephanie Oh <so3756@princeton.edu>
2024-05-06 15:39:45 -0400

    removed decimals from cal goals

--- static/js/validation.js
+++ static/js/validation.js
@@ -1,8 +1,23 @@
-// Validation functions for all inputs: Serving sizes and calorie goals
-// Used in logmeals.html, settings.html, welcome.html
+/* Validation function for all calorie goal inputs.
+The user is only allowed to type positive whole numbers (digits 0-9).
+This function is used in welcome.html and settings.html. */
 
-// Validate the serving size inputs, limiting to positive numbers only
-// and only up to 2 decimal places 
+function isCalGoalValid(input) {
+    var charCode = input.which || input.keyCode;
+
+    // if the key is a valid number character
+    if (charCode >= 48 && charCode <= 57)
+        return true;
+
+    return false;
+}
+
+/* Validation function for all serving size inputs.
+The user is only allowed to type up to two decimal places. 
+The user is also limited to numerical characters 
+(digits 0-9) and the decimal point (.). This function is used in 
+logmeals.html, editingplate.html, and custom_add.html. */
+
 function isValid(input) {
     var charCode = input.which || input.keyCode;
     var value = input.target.value;
@@ -29,7 +44,7 @@ function isValid(input) {
 }
 
 // When the user submits the form and all fields are valid, lock it
-// Used to validate Calorie Goal inputs (settings.html)
+// Used to validate Calorie Goal inputs (settings.html) */
 function closeForm() {
     // Obtain form elements
     // var validFormElements = document.querySelector(":valid")
@@ -44,13 +59,9 @@ function closeForm() {
         var currentNumber = calorieInput.value;
         calorieInput.value = currentNumber.replace(/^0+/, '');
 
-        // Round the number to two decimal places
-        var currentFloat = parseFloat(calorieInput.value);
-
-        if (!isNaN(currentFloat)) {
-            // If it's not NaN, round the float to two decimal places and update the input value
-            calorieInput.value = currentFloat.toFixed(2);
-        }
+        // Round the number to a whole number
+        var currentInt = parseInt(calorieInput.value);
+        calorieInput.value = currentInt;
 
         // Disable the button but have it submit the form
         calorieInput.readOnly = true
