218beb2337702a4a9fb9d5b0dd734cfd0cabfa29
Stephanie Oh <so3756@princeton.edu>
2024-05-06 15:26:53 -0400

    sus validation

--- static/js/validation.js
+++ static/js/validation.js
@@ -28,3 +28,39 @@ function isValid(input) {
     return false;
 }
 
+// When the user submits the form and all fields are valid, lock it
+// Used to validate Calorie Goal inputs (settings.html)
+function closeForm() {
+    // Obtain form elements
+    // var validFormElements = document.querySelector(":valid")
+    // cc
+    var validFormElements = document.querySelector(":valid")
+    var calorieInput = validFormElements.querySelector("#firstCalorieInput")
+    var settingsButton = validFormElements.querySelector("#firstCalorieButton")
+
+    // Check if the input was validated
+    if (calorieInput != null) {
+        // Remove any leading zeros
+        var currentNumber = calorieInput.value;
+        calorieInput.value = currentNumber.replace(/^0+/, '');
+
+        // Round the number to two decimal places
+        var currentFloat = parseFloat(calorieInput.value);
+
+        if (!isNaN(currentFloat)) {
+            // If it's not NaN, round the float to two decimal places and update the input value
+            calorieInput.value = currentFloat.toFixed(2);
+        }
+
+        // Disable the button but have it submit the form
+        calorieInput.readOnly = true
+        settingsButton.style.pointerEvents = "none"
+        settingsButton.textContent = "Submitting..."
+        settingsButton.style.opacity = "var(--bs-btn-disabled-opacity)"
+        settingsButton.tabIndex = "-1"
+        settingsButton.blur()
+        linkPressed()
+    }
+}
+
+
